feb2e0a1a24cab7be26e4e70b7a416c70402ff4d
Sergey <nemuken54@gmail.com>
2024-05-25 21:51:14 +0300

    Initial version

---
+++ libs/algorithms/algorithm.c
@@ -0,0 +1,3 @@
+//
+// Created by Seregro on 25.05.2024.
+//
---
+++ libs/algorithms/algorithm.h
@@ -0,0 +1,8 @@
+//
+// Created by Seregro on 25.05.2024.
+//
+
+#ifndef COURSE_ALGORITHM_H
+#define COURSE_ALGORITHM_H
+
+#endif //COURSE_ALGORITHM_H
